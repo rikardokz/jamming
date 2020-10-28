98de1d709f31d995394b231836813f1f60cda34b
Ricardo <rikardo_kz@live.com.pt>
2020-10-28 19:23:22 +0000

    pass props to Playlist.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -14,6 +14,14 @@ class App extends React.Component{
           {name: "name1", artist: "artist1", album: "album1", id: 1},
           {name: "name2", artist: "artist2", album: "album2", id: 2},
           {name: "name3", artist: "artist3", album: "album3", id: 3}
+        ],
+        playlistName: "Romantic Pimba Love Songs",
+        playlistTracks: 
+        [
+          {name: "playlistName1", artist: "playListArtist1", album: "playlistAlbum1", id: 4},
+          {name: "playlistName2", artist: "playListArtist2", album: "playlistAlbum2", id: 5},
+          {name: "playlistName3", artist: "playListArtist3", album: "playlistAlbum3", id: 6}
+       
         ]
     
     }; 
@@ -27,7 +35,7 @@ class App extends React.Component{
           < SearchBar />
           <div className="App-playlist">
           < SearchResults searchResults={this.state.searchResults}/>
-          < Playlist />
+          < Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
           </div>
         </div>
       </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -8,8 +8,8 @@ export class Playlist extends React.Component {
     render() {
         return (
             <div className="Playlist">
-                <input defaultValue={'New Playlist'}/>
-                
+                <input defaultValue={this.props.playlistName}/>
+                < TrackList tracks={this.props.playlistTracks}/>
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
