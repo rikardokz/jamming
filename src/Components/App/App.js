154d8882cf59e91d68b7b90e59ded3e04d6890aa
Ricardo <rikardo_kz@live.com.pt>
2020-10-30 18:04:24 +0000

    add onSave, to save playlist to spotify

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -39,6 +39,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
@@ -59,7 +60,11 @@ class App extends React.Component {
   }
 
   updatePlaylistName(name) {
-    this.setState( { playlistName: name });
+    this.setState({ playlistName: name });
+  }
+
+  savePlaylist() {
+    const trackUris = this.state.playlistTracks.map((track) => track.uri);
   }
 
   render() {
@@ -80,6 +85,7 @@ class App extends React.Component {
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
               onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
             />
           </div>
         </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -24,7 +24,7 @@ export class Playlist extends React.Component {
           onRemove={this.props.onRemove}
           isRemoval={true}
         />
-        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+        <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
       </div>
     );
   }
