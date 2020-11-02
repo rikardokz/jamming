4ba747c9a65951133f86f896d2964460de27708d
Ricardo <rikardo_kz@live.com.pt>
2020-11-02 18:16:22 +0000

    add search functionality to update state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -40,6 +40,7 @@ class App extends React.Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -67,6 +68,10 @@ class App extends React.Component {
     const trackUris = this.state.playlistTracks.map((track) => track.uri);
   }
 
+  search(term) {
+      console.log(term);
+  }
+
   render() {
     return (
       <div>
@@ -74,7 +79,7 @@ class App extends React.Component {
           Ja<span className="highlight">mmm</span>ing
         </h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search}/>
           <div className="App-playlist">
             <SearchResults
               searchResults={this.state.searchResults}
