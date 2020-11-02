fb8fdfea8ebdea479cf1b2fc97747a6057c2f3dd
Ricardo <rikardo_kz@live.com.pt>
2020-11-02 20:05:50 +0000

    add spotify token verification

---
+++ src/util/Spotify.js
@@ -0,0 +1,28 @@
+let accessToken = "";
+const clientId = "d7306d780f0c41c6aa2986ccfc0abf2a";
+const redirectUri = "http://localhost:3000/";
+
+const Spotify = {
+    getAccessToken() {
+        if(accessToken) {
+            return accessToken;
+        } 
+
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if(accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+
+            window.setTimeout(() => accessToken = "", expiresIn * 1000);
+            window.history.pushState("Access Token", null, "/")
+            return accessToken;
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location.href = accessUrl;
+        }
+    }
+};
+
+export default Spotify;
